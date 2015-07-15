e04bd7e12f41448430d13afc5963e48b50aedc65
woeishi <woei@refect.net>
2015-07-15 21:41:21 +0200

    filled in name in AssemblyInfo

--- plugins/MQTT/Properties/AssemblyInfo.cs
+++ plugins/MQTT/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DemoPlugin")]
+[assembly: AssemblyTitle("MQTT VVVV Plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DemoPlugin")]
+[assembly: AssemblyProduct("MQTT Plugin")]
 [assembly: AssemblyCopyright("Copyright 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
